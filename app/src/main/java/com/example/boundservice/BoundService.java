cd138cfcd63fbe2988889f3f0f0cf4b8645ebb27
Evin1- <edwin.hdz.12.0@gmail.com>
2016-04-06 01:04:52 -0400

    Add binding logic in MainActivity

--- app/src/main/java/com/example/boundservice/BoundService.java
+++ app/src/main/java/com/example/boundservice/BoundService.java
@@ -4,9 +4,11 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
 
 public class BoundService extends Service {
 
+    private static final String TAG = "BoundServiceTAG_";
     private final IBinder mBinder = new LocalBinder();
 
     public BoundService() {
@@ -21,5 +23,9 @@ public class BoundService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return mBinder;
+    }
+
+    public void doMagic(){
+        Log.d(TAG, "doMagic: ");
     }
 }
--- app/src/main/java/com/example/boundservice/MainActivity.java
+++ app/src/main/java/com/example/boundservice/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.boundservice;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BoundService mBoundService;
+    private boolean isBound = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this, BoundService.class);
+        bindService(intent, mConnection, BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unbindService(mConnection);
+        isBound = false;
+    }
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            BoundService.LocalBinder binder = (BoundService.LocalBinder) service;
+            mBoundService = binder.getService();
+            isBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            isBound = false;
+        }
+    };
+
 }
