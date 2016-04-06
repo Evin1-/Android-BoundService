cdc9826e50fc63cf4d5841d6e196a58b317a411d
Evin1- <edwin.hdz.12.0@gmail.com>
2016-04-06 00:58:26 -0400

    Add Binder implementation

--- app/src/main/java/com/example/boundservice/BoundService.java
+++ app/src/main/java/com/example/boundservice/BoundService.java
@@ -2,15 +2,24 @@ package com.example.boundservice;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
 public class BoundService extends Service {
+
+    private final IBinder mBinder = new LocalBinder();
+
     public BoundService() {
+    }
+
+    public class LocalBinder extends Binder {
+        BoundService getService() {
+            return BoundService.this;
+        }
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder;
     }
 }
